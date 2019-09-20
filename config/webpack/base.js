cb9b8f99fc33a1f28168a4d8c9350e6ac32c0cf4
Janet Quispe <janetquispe@MacBook-Air-de-Janet.local>
2019-09-20 14:50:37 -0500

    feat(home): configuración en prod

---
+++ config/webpack/base.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const pathClient = path.join(__dirname, '../../app/');
+console.log(pathClient + 'src/index.tsx');
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    'app': path.join(pathClient, 'src/index.tsx')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@app': pathClient
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|svg|woff2|woff|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(pathClient, 'public/index.html')
+    }),
+    // new CopyWebpackPlugin([{
+    //   from: './src/img',
+    //   to: ''
+    // }]), 
+  ]
+}
---
+++ config/webpack/prod.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./base.js');
+const outputPath = path.join(__dirname, '../../');
+console.log(outputPath +'dist/')
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    path: path.join(outputPath, 'dist/'),
+    filename:'[name].[hash].js',
+    chunkFilename: '[name].[hash].js'
+  },
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all',
+    }
+  }
+});
